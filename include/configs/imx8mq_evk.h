5b01d8557c3ceef5d0584c52364128d65891c240
frank_chiang <frank_chiang@asus.com>
2021-05-18 16:28:44 +0800

    Append cmdline according to cmdline.txt.
    Move cmdline bootargs setting before autoboot.
    
    Change-Id: I6413ce1fc0bf25a762951c8bf4543a8165c677e0

--- include/configs/imx8mq_evk.h
+++ include/configs/imx8mq_evk.h
@@ -102,6 +102,7 @@
 	"splashimage=0x50000000\0" \
 	"console=ttymxc0,115200\0" \
 	"conf_addr=0x40000000\0"		\
+	"cmdline_addr=0x41000000\0"		\
 	"fdt_overlay_addr=0x42000000\0"		\
 	"fdt_addr_r=0x43000000\0"		\
 	"fdt_addr=0x43000000\0"			\
