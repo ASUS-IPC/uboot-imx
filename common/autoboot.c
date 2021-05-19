e48abb5520a66d3967ff47f8302e861aace1d971
frank_chiang <frank_chiang@asus.com>
2021-05-19 15:10:37 +0800

    Check and change the bits of the PARTITION_CONFIG field of mmc 0:1.
    Change-Id: I3212995601032ee61247cad420365484823272a0

--- common/autoboot.c
+++ common/autoboot.c
@@ -452,6 +452,8 @@ static int abortboot(int bootdelay)
 {
 	int abort = 0;
 
+	run_command("mmc partconf-check 0 0 1 0", 0);
+
 	if (bootdelay >= 0) {
 		if (IS_ENABLED(CONFIG_AUTOBOOT_KEYED))
 			abort = abortboot_key_sequence(bootdelay);
