1893a37a9cde8ab38a50d35a402066f513140666
frank_chiang <frank_chiang@asus.com>
2021-05-21 16:27:28 +0800

    Set GPIO5_IO04 to high.
    GPIO5_IO04:	Write Control for EEPROM.
    
    Change-Id: I2efa151297267538fd329737118c30ef1d9b0545

--- common/autoboot.c
+++ common/autoboot.c
@@ -23,6 +23,7 @@
 #include <linux/delay.h>
 #include <u-boot/sha256.h>
 #include <bootcount.h>
+#include <asm/gpio.h>
 #ifdef is_boot_from_usb
 #include <env.h>
 #endif
@@ -36,6 +37,8 @@ DECLARE_GLOBAL_DATA_PTR;
 #endif
 #define debug_bootkeys(fmt, args...)		\
 	debug_cond(DEBUG_BOOTKEYS, fmt, ##args)
+
+#define GPIO5_IO04 132
 
 static char bootargs[1024] = {0};
 
@@ -546,6 +549,9 @@ const char *bootdelay_process(void)
 void autoboot_command(const char *s)
 {
 	debug("### main_loop: bootcmd=\"%s\"\n", s ? s : "<UNDEFINED>");
+
+	gpio_request(GPIO5_IO04, "EEPROM_WRITE_CONTROL");
+	gpio_direction_output(GPIO5_IO04, 1);
 
 	set_bootargs();
 	parse_cmdline();
