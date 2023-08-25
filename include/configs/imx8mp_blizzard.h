f84a13d6b7749e051c5018f3694587215fa0e0d6
frank_chiang <frank_chiang@asus.com>
2023-08-25 15:56:37 +0800

    Enter fastboot mode when loadimage fail
    Change-Id: I4849228431ea181a61d552efac9415f911ee6749

--- include/configs/imx8mp_blizzard.h
+++ include/configs/imx8mp_blizzard.h
@@ -156,6 +156,8 @@
 	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"fastboot=echo Enter Fastboot Mode ...; " \
+		"fastboot 0;\0" \
 	"netboot=echo Booting from net ...; " \
 		"run netargs;  " \
 		"if test ${ip_dyn} = yes; then " \
@@ -180,7 +182,7 @@
 		   "else " \
 			   "if run loadimage; then " \
 				   "run mmcboot; " \
-			   "else run netboot; " \
+			   "else run fastboot; " \
 			   "fi; " \
 		   "fi; " \
 	   "fi;"
