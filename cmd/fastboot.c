32a4e5f6eab932497db40dcc40af4a9321597289
singhome1_lee <singhome1_lee@asus.com>
2024-08-09 13:58:51 +0800

    Set GPIO1_IO13 to low in fastboot mode.
    GPIO1_IO13: SoC ready to MCU.
    low: SoC is ready
    high: Soc is not ready
    
    Change-Id: I927d46e03d2cdb429068764eecc3ce1f71ee0d13

--- cmd/fastboot.c
+++ cmd/fastboot.c
@@ -16,6 +16,8 @@
 #include <watchdog.h>
 #include <linux/printk.h>
 #include <linux/stringify.h>
+#include <asm/gpio.h>
+#define GPIO1_IO13 13
 
 static int do_fastboot_udp(int argc, char *const argv[],
 			   uintptr_t buf_addr, size_t buf_size)
@@ -110,6 +112,9 @@ static int do_fastboot_usb(int argc, char *const argv[],
 		ret = CMD_RET_FAILURE;
 		goto exit;
 	}
+
+	gpio_request(GPIO1_IO13, "SOC_READY");
+	gpio_direction_output(GPIO1_IO13, 0);
 
 	while (1) {
 		if (g_dnl_detach())
