81538a693bd95c441b943316fd77b2001187ed77
singhome1_lee <singhome1_lee@asus.com>
2024-08-09 13:54:01 +0800

    uboot-imx: bringup Rohm PMIC for ASUS IMX8P-IM-A / PE100A / PV100A
    Change-Id: Icea6cb5d02c378d56fa385461cedd671466afee2

--- include/configs/imx8mq_pe100a.h
+++ include/configs/imx8mq_pe100a.h
@@ -18,7 +18,7 @@
 #define CFG_MALLOC_F_ADDR		0x182000
 /* For RAW image gives a error info not panic */
 
-#define CFG_POWER_PFUZE100_I2C_ADDR 0x08
+#define CONFIG_POWER_BD71837
 #endif
 
 /* ENET Config */
