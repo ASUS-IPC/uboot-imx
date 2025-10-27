b576e1a228a7b61cddba5dc51d843e19a1fd6298
frank_chiang <frank_chiang@asus.com>
2025-10-27 15:30:33 +0800

    Enter fastboot mode when kernel image not found.
    Change-Id: Ie7c059e1188b2abd4c874f694cf36a39bb475c27

--- include/configs/imx8mq_pv100a.h
+++ include/configs/imx8mq_pv100a.h
@@ -112,6 +112,8 @@
 	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"fastboot=echo Enter Fastboot Mode ...; " \
+		"fastboot 0;\0" \
 	"netboot=echo Booting from net ...; " \
 		"run netargs;  " \
 		"if test ${ip_dyn} = yes; then " \
@@ -136,7 +138,7 @@
 			   "else " \
 				   "if run loadimage; then " \
 					   "run mmcboot; " \
-				   "else run netboot; " \
+				   "else run fastboot; " \
 				   "fi; " \
 			   "fi; " \
 		   "fi;"
